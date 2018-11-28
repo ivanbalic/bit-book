a8ea349c400d6afcd824f62ff48157271182d8ca
bit student <student@bit.com>
2018-11-28 17:12:39 +0100

    [bbkt-4] add bootstrap, jquery, popper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './view/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
+ReactDOM.render(<App />, document.getElementById("root"));
