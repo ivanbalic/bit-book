58ffada805aa6ef89d85dad7a590c50536eee275
bit student <student@bit.com>
2018-11-29 15:36:38 +0100

    [book-2] commit fix

--- src/view/components/CreatePost/Modals/TextModal.js
+++ src/view/components/CreatePost/Modals/TextModal.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const TextModal = () => {
-    return (
-        <>
-            <div class="modal fade" id="textModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">New text post</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <input type="text" class="form-control col-11 m-3" placeholder="Post text" aria-label="Username" />
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Post text</button>
+class TextModal extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: ""
+        }
+    }
+
+    getInputValue = (event) => {
+
+        console.log(event.target.value);
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+
+    render() {
+
+        return (
+            <>
+                <div class="modal fade" id="textModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">New text post</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <input type="text" class="form-control col-11 m-3" value={this.state.inputValue} onChange={this.getInputValue} placeholder="Post text" aria-label="Username" />
+                            <p>alert</p>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary">Post text</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </>
-    );
+            </>
+        );
+
+    }
 }
 
 export {
