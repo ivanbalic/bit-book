8ccf8a9df90f2cc5fd100fd80ac79742d89fb9e8
ivanbalic <ivanbalic2@gmail.com>
2019-01-12 14:32:19 +0100

    add login and register heading text change on tab select

--- src/view/pages/LoginAndRegister/LoginAndRegister.js
+++ src/view/pages/LoginAndRegister/LoginAndRegister.js
@@ -27,9 +27,9 @@ class LoginAndRegister extends Component {
     render() {
         return (
             <>
-                <div className="row mt-5">
+                <div className="row m-5">
                     <div className="col-5">
-                        <h3>{this.state.isLogin ? "BitBook Login" : "BitBook Register"}</h3>
+                        <h3>{this.state.isLoginTabActive ? "BitBook Login" : "BitBook Register"}</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
                     <div className="offset-1 col-6">
@@ -38,9 +38,7 @@ class LoginAndRegister extends Component {
                             <p className={this.state.isLoginTabActive ? "nav-item nav-link col-6" : "nav-item nav-link active col-6"} onClick={this.onRegisterTabSelect}>Register</p>
                         </nav>
 
-                        {
-                            this.state.isLoginTabActive ? <Login loginStatusCallback={this.props.loginStatusCallback} /> : <Register />
-                        }
+                        {this.state.isLoginTabActive ? <Login /> : <Register />}
                     </div>
                 </div>
             </>
